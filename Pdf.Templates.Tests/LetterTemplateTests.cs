7e88405aecd2fe2078aca2bb82b3a1c84d4b6758
DerHulk <thegrayhulk@googlemail.com>
2017-08-11 17:33:06 +0200

    Initalize Project.

---
+++ Pdf.Templates.Tests/LetterTemplateTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pdf.Templates.Tests
+{
+    public class LetterTemplateTests
+    {
+        [Fact]
+        public void Write01()
+        {
+            //arrange
+            var target = new LetterTemplate();
+
+            //act
+            target.Write();
+
+            //assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ Pdf.Templates/LetterTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdf.Templates
+{
+    public class LetterTemplate
+    {
+        public void Write()
+        {
+            //do nothing;
+        }
+    }
+}
