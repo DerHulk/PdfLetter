be95d3537d7d3ab4a4bdaab00271f3b0c6272ae3
DerHulk <thegrayhulk@googlemail.com>
2017-08-12 14:11:30 +0200

    Add first pdf sample.

--- Pdf.Templates.Tests/LetterTemplateTests.cs
+++ Pdf.Templates.Tests/LetterTemplateTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,10 +17,15 @@ namespace Pdf.Templates.Tests
             var target = new LetterTemplate();
 
             //act
-            target.Write();
+            var result = target.Write();
+            var fileName = System.IO.Path.GetTempFileName() + ".pdf";
 
-            //assert
-            Assert.True(true);
+            System.IO.File.WriteAllBytes(fileName, result);
+
+            //assert                
+            Process.Start(fileName);
+            System.IO.File.Delete(fileName);
+
         }
     }
 }
--- Pdf.Templates/LetterTemplate.cs
+++ Pdf.Templates/LetterTemplate.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 
 namespace Pdf.Templates
 {
     public class LetterTemplate
     {
-        public void Write()
+        public byte[] Write()
         {
-            //do nothing;
+            var stream = new MemoryStream();
+
+            using (var document = new PdfDocument())
+            {
+                XFont font = new XFont("Times", 25, XFontStyle.Bold);
+
+                PdfPage page = document.AddPage();
+                page.Size = PageSize.A4;
+
+                XGraphics gfx = XGraphics.FromPdfPage(page);
+
+                gfx.DrawString("Hallo Welt", font, XBrushes.DarkRed, new XRect(0, 0, page.Width, page.Height), XStringFormat.Center);
+
+                document.Save(stream);
+            }
+
+            return stream.ToArray();
         }
     }
 }
